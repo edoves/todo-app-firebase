c1b84a530af7be2210689c9697c314233dff7d3b
dove <ndoverte11@gmail.com>
2021-04-03 10:40:04 +0800

    Setting app intial code for app and index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./app.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="todo">
+      <div className="container"></div>
     </div>
   );
 };
